92b83b35b6d5cc346c0cdb25822ea3598f39700e
Suman <pasisuman2511@gmail.com>
2025-09-21 11:41:37 +0530

    Login textinput,signup,signin,clear created

--- ATM_automation_system/src/atm_automation_system/Login.java
+++ ATM_automation_system/src/atm_automation_system/Login.java
@@ -17,7 +17,47 @@ public class Login extends JFrame{
         label.setBounds(70, 10, 100, 100);
         add(label);
         
+        JLabel text = new JLabel("Welcom to ATM");
+        text.setFont(new Font ("Osward", Font.BOLD, 38));
+        text.setBounds(200, 40,400,40);
+        add(text);
+        
+        JLabel cardno = new JLabel("Card No:");
+        cardno.setFont(new Font ("Raleway", Font.BOLD, 28));
+        cardno.setBounds(120,150,150,30);
+        add(cardno);
+        
+        JTextField cardTextField = new JTextField();
+        cardTextField.setBounds(300, 150, 230, 30);
+        add(cardTextField);
+        
+        JLabel pin = new JLabel("PIN:");
+        pin.setFont(new Font ("Raleway", Font.BOLD, 28));
+        pin.setBounds(120,220,250,30);
+        add(pin);
+        
+        JTextField pinTextField = new JTextField();
+        pinTextField.setBounds(300, 220, 230, 30);
+        add(pinTextField);
         getContentPane().setBackground(Color.WHITE);
+        
+        JButton login = new JButton("SIGN IN");
+        login.setBounds(300, 300, 100, 30);
+        login.setBackground(Color.BLACK);
+        login.setForeground(Color.WHITE);
+        add(login);
+        
+        JButton clear = new JButton("CLEAR");
+        clear.setBounds(430, 300, 100, 30);
+        clear.setBackground(Color.BLACK);
+        clear.setForeground(Color.WHITE);
+        add(clear);
+        
+        JButton signup = new JButton("SIGN UP");
+        signup.setBounds(300, 350, 230, 30);
+        signup.setBackground(Color.BLACK);
+        signup.setForeground(Color.WHITE);
+        add(signup);
         
         setSize(800, 480);
         setVisible(true);
